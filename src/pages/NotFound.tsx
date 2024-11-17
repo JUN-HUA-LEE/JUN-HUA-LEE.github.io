3938641af3ef46da9117d3db9f4ffeca02741cd1
Jun-Hua-Lee <128239740+JUN-HUA-LEE@users.noreply.github.com>
2024-11-17 15:35:11 +0800

    Add NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen text-4xl">
+      404 - Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
