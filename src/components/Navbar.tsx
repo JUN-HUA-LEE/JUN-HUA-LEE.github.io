6d4551de781423983f87167f88f81a3ec66e47d0
Jun-Hua-Lee <128239740+JUN-HUA-LEE@users.noreply.github.com>
2024-12-08 22:32:36 +0800

    Deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,12 @@
 import { useState, useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
-import { motion } from 'framer-motion';
+import { FaGraduationCap, FaBriefcase } from 'react-icons/fa';
 
 const Navbar = () => {
-  const [show, setShow] = useState(true);
   const [atTop, setAtTop] = useState(true);
   let lastScrollY = window.scrollY;
 
   const controlNavbar = () => {
-    if (window.scrollY > lastScrollY) {
-      setShow(false);
-    } else {
-      setShow(true);
-    }
-    lastScrollY = window.scrollY;
     setAtTop(window.scrollY === 0);
   };
 
@@ -24,44 +17,38 @@ const Navbar = () => {
     };
   }, []);
 
-  const underlineVariants = {
-    hidden: { width: 0, left: '50%' },
-    visible: { width: '100%', left: 0, transition: { duration: 0.3 } },
-  };
-
   return (
-    <motion.nav
-      initial={{ y: 0 }}
-      animate={{ y: show ? 0 : -100 }}
-      transition={{ duration: 0.5 }}
-      className={`fixed top-0 left-0 w-full py-4 px-6 z-50 flex items-center justify-center shadow-md transition-all duration-300 ${
-        atTop ? 'bg-transparent' : 'bg-black bg-opacity-50'
-      }`}
-    >
-      <div className="flex items-center gap-8">
-        {['Experience', 'Education'].map((item, index) => (
-          <motion.div
-            key={index}
-            className="relative font-bold text-2xl text-white"
-            initial="hidden"
-            whileHover="visible"
+    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 z-50 px-4 sm:px-0">
+      <nav
+        className={`flex items-center justify-between shadow-md rounded-full border border-neutral-700 py-2 px-6 transition-all duration-300 ${
+          atTop ? 'bg-transparent' : 'bg-black bg-opacity-50'
+        }`}
+      >
+        <div className="font-bold text-md text-white">
+          Jun Hua Lee
+        </div>
+        <div className="flex items-center gap-4 font-semibold text-md text-neutral-400">
+          <ScrollLink
+            to="experience"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer flex items-center gap-2"
           >
-            <ScrollLink
-              to={item.toLowerCase()}
-              smooth={true}
-              duration={500}
-              className="cursor-pointer relative"
-            >
-              {item}
-              <motion.div
-                className="absolute bottom-[-8px] left-0 h-[2px] bg-white"
-                variants={underlineVariants}
-              />
-            </ScrollLink>
-          </motion.div>
-        ))}
-      </div>
-    </motion.nav>
+            <span className="hidden sm:block">Experience</span>
+            <FaBriefcase className="block sm:hidden" />
+          </ScrollLink>
+          <ScrollLink
+            to="education"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer flex items-center gap-2"
+          >
+            <span className="hidden sm:block">Education</span>
+            <FaGraduationCap className="block sm:hidden" />
+          </ScrollLink>
+        </div>
+      </nav>
+    </div>
   );
 };
 
